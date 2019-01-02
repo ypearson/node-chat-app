6983ced02406020e1ea3f57cec6c01d9a22c4ab3
Yvan Pearson <ypearson@nklabs.com>
2019-01-02 12:51:56 -0500

    basic example using sockets io

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path    = require('path');
+const http    = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -9,13 +11,21 @@ console.log(publicPath);
 const port = process.env.PORT || 3000;
 
 var app = express();
-
-// app.get('/',function(req,res){
-//   res.sendFile(publicPath +'/index.html');
-// });
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+
+    console.log("user connection")
+
+    socket.on('disconnect', () => {
+      console.log('User was disconnected from server');
+    });
+});
+
+
+server.listen(port, () => {
   console.log(`listening on ${port}`);
 });
